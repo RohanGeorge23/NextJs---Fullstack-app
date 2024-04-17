38c0c8e1734f2bedc937984b90ad62af6c98c48d
Rohan George <rohangeorge77@gmail.com>
2024-04-17 09:51:35 +0530

    added POST req for signup

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -4,9 +4,20 @@
 import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
 
+
+
 export function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    async function  clickHandler(){
+       let data =await axios.post('http://localhost:3000/api/users',
+       {
+        username,
+        password
+       })
+        console.log('dataaaaaa',data)
+    }
 
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
@@ -24,7 +35,7 @@ export function Signup() {
                         <LabelledInput onChange={(e) => {
                             setPassword(e.target.value)
                         }} label="Password" type={"password"} placeholder="123456" />
-                        <button type="button"  className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                        <button onClick={clickHandler} type="button"  className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
                 </div>
             </a>
